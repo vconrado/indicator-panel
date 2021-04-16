1097fc9d760c779c07946284a9c2265a76893257
André Carvalho <afacarvalho@yahoo.com.br>
2021-04-15 21:33:20 -0300

    Change the height to components box in main row.

--- js/utils.js
+++ js/utils.js
@@ -1,18 +1,12 @@
 var utils={
     /**
-     * Apply a percentage of the total height to main map component.
+     * Apply a percentage of the total height to components in main row where is the map component.
      * @param {double} offset, a percentage value between 0 and 1, default 0.6(60%)
      */
-    setMapHeight:(offset)=>{
+    setMainRowHeight:(offset)=>{
         if(!offset) offset=0.6;
-        let mapHeight=window.innerHeight*offset;
-        $('#mainmap').height(mapHeight);
-    },
-
-    /**
-     * Load a CSV data using background request
-     */
-    getCSV:(url)=>{
-
+        let mainRowHeight=window.innerHeight*offset;
+        $('#mainmap').height(mainRowHeight);
+        $('#treeview').height(mainRowHeight);
     }
 };
